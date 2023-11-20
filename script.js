c14bfb7b77cb6cd7817d4465303b97a095bdc1b9
Nilson Cesar <admncoaf@gmail.com>
2023-11-20 15:07:37 -0300

    Create function that make grid of divs

---
+++ script.js
@@ -0,0 +1,17 @@
+let makeDivGrid = (n, m) => {
+    let grid = document.querySelector('.sketch');
+    for(let i = 0; i < n; i++) {
+        let line = document.createElement('div');
+        line.classList.add('.sketch-line');
+        for(let j = 0; j < m; j++) {
+            let item = document.createElement('div');
+            item.textContent = 'test';
+            item.classList.add('.sketch-item');
+            line.appendChild(item);
+        }
+
+        grid.appendChild(line);
+    }
+}
+
+makeDivGrid(16, 16);
